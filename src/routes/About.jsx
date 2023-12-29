ec126c47a00179ea4617d26bf74e1a706ee5e73b
Difficins <a.kolomijec@seznam.cz>
2023-12-29 13:52:16 +0100

    Added source

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -11,6 +11,10 @@ function About() {
         &nbsp;course.
       </div>
       <div>
+        Fake store data by&nbsp;
+        <a href="https://fakestoreapi.com/">Fakestoreapi</a>.
+      </div>
+      <div>
         Background picture from&nbsp;
         <a href="https://www.freepik.com/free-photo/black-friday-elements-assortment_17658453.htm#query=ecommerce%20background&position=3&from_view=keyword&track=ais&uuid=6971a8c9-2fde-407d-a07d-24a60a7f60fb">
           Freepik
